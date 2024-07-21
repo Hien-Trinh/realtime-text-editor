a4d3899caa06d8a4ecee581f9195345330e0a62b
Hien-Trinh <hientrinhminh32@gmail.com>
2024-07-22 00:13:44 +0700

    added websocket and onTextChange methods

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
+import { io } from "socket.io-client"
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -13,12 +14,46 @@ const TOOLBAR_OPTIONS = [
     ["image", "blockquote", "code-block"],
     ["clean"],
 ]
+const MODULES = {
+    toolbar: TOOLBAR_OPTIONS,
+}
 
 export default function TextEditor() {
     const [value, setValue] = useState("")
+    const [socket, setSocket] = useState()
+
+    useEffect(() => {
+        const socketInstance = io("http://localhost:3001")
+        setSocket(socketInstance)
+
+        return () => {
+            socketInstance.disconnect()
+        }
+    }, [])
+
+
+    useEffect(() => {
+        if (socket == null) return
+        socket.on("receive-changes", delta => {
+            setValue(delta)
+        })
+        return () => {
+            socket.off("receive-changes")
+        }
+    }, [socket, value])
+
+    const handleChange = (content, delta, source, editor) => {
+        if (source !== "user") return
+    }
+
     return (
         <div className="container">
-            <ReactQuill theme="snow" value={value} onChange={setValue} />
+            <ReactQuill
+                theme="snow"
+                modules={MODULES}
+                value={value}
+                onChange={(content, delta, source, editor) => handleChange(content, delta, source, editor)}
+            />
         </div>
     )
 }
